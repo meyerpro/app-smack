03059cd6e654cb16ecc4c940008e8331b557e3f9
Derek Meyer <meyerpro@gmail.com>
2018-11-02 03:15:15 -0700

    Lesson-09 complete

--- app-smack/app-smack/Utilities/Constants.swift
+++ app-smack/app-smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://app-smack-meyerpro.herokuapp.com/v1/"
+let URL_REGISTER = BASE_URL + "account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAcct"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
